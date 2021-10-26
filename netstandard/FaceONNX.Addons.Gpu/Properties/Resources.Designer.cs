47879802324a082fee4727a432f6b09daa0919cc
asiryan <dmc5mod@yandex.ru>
2021-10-26 16:45:58 +0300

    properties fixtures

--- netstandard/FaceONNX.Addons.Gpu/Properties/Resources.Designer.cs
+++ netstandard/FaceONNX.Addons.Gpu/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FaceONNX.Addons.Gpu.Properties {
+namespace FaceONNX.Addons.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FaceONNX.Addons.Gpu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FaceONNX.Addons.Gpu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FaceONNX.Addons.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
